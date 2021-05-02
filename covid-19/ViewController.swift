ff79d418fb0235db4f04111920941a287b175cc7
Amro <amrmu94@gmail.com>
2021-05-02 15:43:43 -0700

    Initial Commit

---
+++ covid-19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  covid-19
+//
+//  Created by Amr Muhammad on 5/2/21.
+//  Copyright © 2021 Amr Muhammad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
